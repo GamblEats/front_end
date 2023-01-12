5c76d62db1affaa3be98c207c629e1b9e2579f13
Steven Lucas <steven.lucas2201@gmail.com>
2023-01-12 14:11:20 +0100

    icons & categories

---
+++ config/categories.ts
@@ -0,0 +1,58 @@
+import Pasta from '../public/pictures/icons/spaguetti.png'
+import Burger from '../public/pictures/icons/hamburger.png'
+import Pizza from '../public/pictures/icons/pizza.png'
+import Salad from '../public/pictures/icons/salad.png'
+import Chicken from '../public/pictures/icons/chicken.png'
+import Mexican from '../public/pictures/icons/taco.png'
+import Thai from '../public/pictures/icons/thailand.png'
+import Chinese from '../public/pictures/icons/dragon.png'
+import Japanese from '../public/pictures/icons/japanese.png'
+import Shopping from '../public/pictures/icons/shopping.png'
+import Sandwich from '../public/pictures/icons/sandwich.png'
+
+export const categories = [
+    {
+        name: "Pasta",
+        pic: Pasta
+    },
+    {
+        name: "Burger",
+        pic: Burger
+    },
+    {
+        name: "Pizzas",
+        pic: Pizza
+    },
+    {
+        name: "Salads",
+        pic: Salad
+    },
+    {
+        name: "Chicken",
+        pic: Chicken
+    },
+    {
+        name: "Mexican",
+        pic: Mexican
+    },
+    {
+        name: "Thaï",
+        pic: Thai
+    },
+    {
+        name: "Chinese",
+        pic: Chinese
+    },
+    {
+        name: "Japanese",
+        pic: Japanese
+    },
+    {
+        name: "Shopping",
+        pic: Shopping
+    },
+    {
+        name: "Sandwiches",
+        pic: Sandwich
+    },
+]
